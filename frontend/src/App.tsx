6db9b40d323c4fdcef2c3a2daae4067dd8094de6
moha <mohammedkhettala@gmail.com>
2025-06-21 20:50:05 +0200

    [PROJECT-MANAGEMENT-UI] FRONTEND SET UP

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
+
+function App() {
+
+  return (
+    <Container>
+      <Typography>hello</Typography>
+    </Container>
+  )
+}
+
+export default App
